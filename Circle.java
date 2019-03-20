429e6d2e5e7f913402e33e0d7e63630d424a5943
Shravani1510 <shravanikumari321@gmail.com>
2019-03-19 19:30:48 -0700

    please check 2task

--- Circle.java
+++ Circle.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
 
 public class Circle {
 	//todo: add some methods to compute a circle's diameter, circumference and area.
 	//print some outputs to confirm you methods are working right.
+	//diameter 2r; area of circle pir^2; circumference 2* pi* r;
+	static Scanner scan = new Scanner(System.in);
+	static float r = scan.nextInt();
+
+	
+	public static double diameter() {
+		return 2* r;
+	}
+	public static double circumference() {
+		return 2*Math.PI* r ;
+	}
+	public static double area() {
+		return  Math.PI*r*r;
+	}
+	public static void main(String[] args) {
+		double d = diameter();
+		double c = circumference();
+		double A = area();
+		System.out.println("diameter of a circle ="+ d);
+		System.out.println("circumference of a circle ="+ c);
+		System.out.println(" area of a circle = "+ A );
+	}
+	
+	
+	
 }
+
--- Prime.java
+++ Prime.java
@@ -10,6 +10,35 @@ public class Prime {
 	//The function should return the smaller one of the doublette. If there exists no
 	//prime doublette between the given parameter and the maximal int-value (Integer.MAX_VALUE)
 	//the function should return 0.
+	static Scanner scan = new Scanner(System.in);
+	static int n = scan.nextInt();
+	public static boolean isprime(int n) {
+		for(int i=2; i<n; i++) {
+	 if(n%i==0) {
+		return false;
+	 		}
+		}
+		return true;
+	}
+	public static int findDoublette(int n) {
 		
+		for(int i= n; i<Integer.MAX_VALUE; i++) {
+			boolean r = isprime(i);
+			if(r) {
+				int j=i+2;
+				boolean r1= isprime(j);
+				if(r1) {
+				return i;
+				}
+			}
+		}
+		return -1;
+		
+	}
+	public static void main(String[] args) {
+		isprime(n);
+		System.out.println(isprime(n));
+		System.out.println(findDoublette(n));
+	}
 	
 }
